71847a226749032725eb51dc888ce0b060c6ced5
anl331 <anatal31@gmail.com>
2023-06-05 23:03:26 -0700

    data fecth and stored on useState = fixes

--- store/useAppStore.ts
+++ store/useAppStore.ts
@@ -1,11 +1,11 @@
 import getAllClients from "@/lib/getAllClients"
 import { Client, User } from "@/typings"
 import { create } from "zustand"
-import { persist, devtools } from "zustand/middleware"
+import { persist, devtools, createJSONStorage } from "zustand/middleware"
 
 type ClientStore = {
 	currentUserId: number | null
-	currentUser: number | null
+	currentUser: {} | null | any[]
 	isLoading: boolean
 	error: null
 	clients: any[] | null
@@ -14,7 +14,7 @@ type ClientStore = {
 type Actions = {
 	getClients: () => void
 	setcurrentUserId: (id: number) => void
-	setcurrentUser: (user: User) => void
+	setCurrentUser: (currentUser: any) => void
 }
 
 const clientStore = (set) => ({
@@ -34,16 +34,39 @@ const clientStore = (set) => ({
 	setcurrentUserId: (id) => {
 		set({ currentUserId: id })
 	},
-	setCurrentUser: (user: User) => {
-		set({ currentUser: user })
+	setCurrentUser: (currentUser ) => {
+		set({ currentUser })
 	},
 })
 
 export const useAppStore = create<ClientStore & Actions>(
 	devtools(
-		persist(clientStore, {
-			name: "session",
-			storage,
-		})
+		persist(
+			(set) => ({
+				currentUserId: null,
+				currentUser: null,
+				isLoading: true,
+				error: null,
+				clients: [],
+				getClients: async () => {
+					try {
+						const allClients = await getAllClients()
+						set({ isLoading: false, clients: allClients })
+					} catch (error) {
+						set({ error: error.message, isLoading: false })
+					}
+				},
+				setcurrentUserId: (id) => {
+					set({ currentUserId: id })
+				},
+				setCurrentUser: (user: User) => {
+					set({ currentUser: user })
+				},
+			}),
+			{
+				name: "session",
+				storage: createJSONStorage(() => sessionStorage),
+			}
+		)
 	)
 )
